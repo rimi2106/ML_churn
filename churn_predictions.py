7163f52d0ec94f5a1ff26d468ce6d24687b954a2
rimi2106 <nandy.rimi@gmail.com>
2025-02-11 12:52:33 -0500

    Update churn_predictions.py

--- churn_predictions.py
+++ churn_predictions.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-#import streamlit as st
+import streamlit as st
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
